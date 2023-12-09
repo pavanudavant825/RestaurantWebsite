15baff03f45f993a7fb21caa823ab2aff8c43636
pavanudavant825 <pavanudavant66@gmail.com>
2023-12-09 15:18:36 +0530

    first commit

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,59 @@
+const Menu = [
+{
+    id:1,
+    image:"images/maggi.jpg",
+    name:"Maggi",
+    category:"lunch",
+    price: "12 rs",
+    description:"I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sint natus magni deserunt dolore. Eius mollitia modi, doloremque, dolorem quasi, beatae dolorum laborum ducimus vitae odio quas iure cupiditate impedit. ",
+},
+
+{
+    id:2,
+    image:"images/alupakoda.jpg",
+    name:"Alu Pakoda",
+    category:"evening",
+    price:"30 Rs",
+    description:"I love alupakoda really Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sint natus magni deserunt dolore. Eius mollitia modi, doloremque, dolorem quasi, beatae dolorum laborum ducimus vitae odio quas iure cupiditate impedit. ",
+},
+
+{
+    id:3,
+    image:"images/samosa.jpg",
+    name:"Samosa",
+    category:"breakfast",
+    price:"20 Rs",
+    description:"I love samosa really Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sint natus magni deserunt dolore. Eius mollitia modi, doloremque, dolorem quasi, beatae dolorum laborum ducimus vitae odio quas iure cupiditate impedit. ",
+},
+
+{
+    id:4,
+    image:"images/momos.jpg",
+    name:"Momos",
+    category:"evening",
+    price:"90 Rs",
+    description:"I love momos really Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sint natus magni deserunt dolore. Eius mollitia modi, doloremque, dolorem quasi, beatae dolorum laborum ducimus vitae odio quas iure cupiditate impedit. ",
+},
+
+{
+    id:5,
+    image:"images/pavbhaji.jpg",
+    name:"Pavbhaji",
+    category:"dinner",
+    price:"100 Rs",
+    description:"I love pavbhaji really Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sint natus magni deserunt dolore. Eius mollitia modi, doloremque, dolorem quasi, beatae dolorum laborum ducimus vitae odio quas iure cupiditate impedit. ",
+},
+
+{
+    id:6,
+    image:"images/pizza.jpg",
+    name:"Pizza",
+    category:"evening",
+    price:"200 Rs",
+    description:"I love pizza really Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem sint natus magni deserunt dolore. Eius mollitia modi, doloremque, dolorem quasi, beatae dolorum laborum ducimus vitae odio quas iure cupiditate impedit. ",
+},
+
+
+
+];
+export default Menu;
