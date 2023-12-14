796e99d04f24de935cdd2354a5c6f0acfef3a9ea
pavanudavant825 <pavanudavant66@gmail.com>
2023-12-14 20:38:46 +0530

    I changed Restaurant.js

--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'
 import "./style.css"
-import Menu from './menuApi'
+import Menu from './menuApi.js'
 import MenuCard from './MenuCard'
 
 
 const Restaurant = () => {
     const [menuData, setMenuData]=useState(Menu); 
-
     const filterItem=(category)=>{
         const updatedList = Menu.filter((curElem)=>{
             return curElem.category === category;
@@ -20,8 +19,8 @@ const Restaurant = () => {
         <button className='btn-group__item' onClick={()=>filterItem("breakfast")}>Breakfast</button>
         <button className='btn-group__item' onClick={()=>filterItem("lunch")}>Lunch</button>
         <button className='btn-group__item' onClick={()=>filterItem("evening")}>Evening</button>
-        <button className='btn-group__item' onClick={()=>setMenuData(Menu)}>Dinner</button>
-        <button className='btn-group__item'>All</button>
+        <button className='btn-group__item' onClick={()=>filterItem("dinner")}>Dinner</button>
+        <button className='btn-group__item' onClick={()=>setMenuData(Menu)}>All</button>
     </div>
    </nav>
    <MenuCard menuData={menuData}/>
